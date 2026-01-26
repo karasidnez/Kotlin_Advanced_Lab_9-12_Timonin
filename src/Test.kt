88d2d1fe72e0fd8ea9bc1acd0b6ba2e06bf7a29b
karasidnez <karasdnez@gmail.com>
2026-01-26 12:49:58 +0300

    Add data class OutpostResource and ResourceManager

--- src/Test.kt
+++ src/Test.kt
@@ -3,11 +3,26 @@ var age: Int = 18
         if ((value > 0) and (value < 110))
             field = value
     }
+data class Item(
+    val id: Int,
+    val name: String,
+    val quantity: Int
+){
+    override fun toString(): String {
+        return "Id предмета: $id\nИмя: $name\nКоличество: $quantity\n"
+    }
+}
 
 fun main() {
-    println(age) // 18
-    age = 45
-    println(age) // 45
-    age = -345
-    println(age) // 45
+//    println(age) // 18
+//    age = 45
+//    println(age) // 45
+//    age = -345
+//    println(age) // 45
+//    val sword = Item(1,"Sword", 1)
+//    val betterSword = sword.copy(quantity = 2)
+//    println(sword.toString())
+//    println(betterSword.toString())
+//    val(id,name,quantity) = betterSword
+//    println("Id предмета: $id\nИмя: $name\nКоличество: $quantity\n")
 }
